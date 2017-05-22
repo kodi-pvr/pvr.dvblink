c36c2f0cd3a15c82321e939c8dba9292e1d6fa3c
Alwin Esch <alwin.esch@web.de>
2017-05-22 04:11:39 +0200

    Update on kodi to addon interface

--- src/client.cpp
+++ src/client.cpp
@@ -405,10 +405,6 @@ ADDON_STATUS ADDON_SetSetting(const char *settingName, const void *settingValue)
   return ADDON_STATUS_OK;
 }
 
-void ADDON_Stop()
-{
-}
-
 /***********************************************************
  * PVR Client AddOn specific public library functions
  ***********************************************************/
